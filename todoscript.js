9b2dbb2c77fe6594753d666a81e14c40985c412f
emiliegre <egrenth@gmail.com>
2021-10-13 13:04:48 +0200

    styled the todolist

--- todoscript.js
+++ todoscript.js
@@ -43,6 +43,7 @@ window.onload = function() {
     function addCheckbox(listElement, status) {
         var chbox = document.createElement("input");
         chbox.type = "checkbox";
+        chbox.id = "chbox"
         if(status === "checked"){
             chbox.checked = true;
         }
@@ -63,10 +64,11 @@ window.onload = function() {
     function addInput(listElement, input) {
         var txt = document.createTextNode(input);
         var p = document.createElement("p");
+        p.id = "txt"
         p.appendChild(txt); 
         p.addEventListener("click", function(e) {
             var oldtxt = e.target.innerText;
-            var newtxt = prompt("Change to do to");
+            var newtxt = prompt("Change to do to", oldtxt);
             if(newtxt) {
                 e.target.innerText = newtxt;
 
